101dbfaa2bace998691150597fd586d2635e5b00
whitegoldukhta <james.99@list.ru>
2022-01-22 10:09:35 +0200

    spring task

--- Spring_2ndTask/src/main/java/app/model/AnimalsCage.java
+++ Spring_2ndTask/src/main/java/app/model/AnimalsCage.java
@@ -1,13 +1,20 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
 
     @Autowired
+    @Qualifier("dog")
     private Animal animal;
+
+    @Autowired
+    @Qualifier("timer")
+    private Timer timer;
+
 
     public void whatAnimalSay() {
         System.out.println("Say:");
@@ -15,5 +22,9 @@ public class AnimalsCage {
         System.out.println("At:");
         System.out.println(new Timer().getTime());
         System.out.println("________________________");
+    }
+
+    public Timer getTimer() {
+        return timer;
     }
 }
